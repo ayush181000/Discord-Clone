1aec1817ba55970fc4f7ee572458c011a3ce8d7e
Ayush Garg <ayush181000@gmail.com>
2022-09-06 03:35:29 +0530

    finish auth routes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
+import { logout } from './shared/utils/auth';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:5002/api',
   timeout: 1000,
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem('user');
+
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 export const login = async (data) => {
   try {
@@ -24,5 +39,15 @@ export const register = async (data) => {
       error: true,
       exception,
     };
+  }
+};
+
+// secure routes
+
+const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+
+  if (responseCode) {
+    (responseCode == 401 || responseCode == 403) && logout();
   }
 };
