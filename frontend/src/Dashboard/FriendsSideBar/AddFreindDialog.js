fc2a17bf79e4ed8c9f4186bb118567cda4a94b7d
Ayush Garg <ayush181000@gmail.com>
2022-09-06 05:43:09 +0530

    sidebar and dialog box

---
+++ frontend/src/Dashboard/FriendsSideBar/AddFreindDialog.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  Typography,
+} from '@mui/material';
+import { validateMail } from '../../shared/utils/validators';
+import InputWithLabel from '../../shared/components/InputWithLabel';
+import CustomPrimaryButton from '../../shared/components/CustomPrimaryButton';
+
+const AddFreindDialog = ({
+  isDialogOpen,
+  closeDialogHandler,
+  sendFriendInvitation = () => {},
+}) => {
+  const [mail, setMail] = useState('');
+  const [isFormValid, setIsFormValid] = useState('');
+
+  const handleSendInvitation = (invitation) => {
+    // TODO:send friend request to server
+  };
+
+  const hadnleCloseDialog = () => {
+    closeDialogHandler();
+    setMail('');
+  };
+
+  useEffect(() => {
+    setIsFormValid(validateMail(mail));
+  }, [mail, setIsFormValid]);
+  return (
+    <div>
+      <Dialog open={isDialogOpen} onClose={hadnleCloseDialog}>
+        <DialogTitle>
+          <Typography>Invite a Friend</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Typography>
+              Enter e-mail address of friend which you would like to invite
+            </Typography>
+          </DialogContentText>
+          <InputWithLabel
+            label='mail'
+            type='text'
+            value={mail}
+            setValue={setMail}
+            placeholder='Enter a e-mail address'
+          />
+        </DialogContent>
+        <DialogActions>
+          <CustomPrimaryButton
+            onClick={handleSendInvitation}
+            disabled={!isFormValid}
+            label='Send'
+            additionalStyles={{
+              marginLeft: '15px',
+              marginRight: '15px',
+              marginBottom: '10px',
+            }}
+          />
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddFreindDialog;
--- frontend/src/shared/utils/validators.js
+++ frontend/src/shared/utils/validators.js
@@ -1,26 +1,24 @@
 export const validateLoginForm = (mail, password) => {
-  const isMailValid = validateMail(mail);
-  const isPasswordValid = validatePassword(password);
-
-  return isMailValid && isPasswordValid;
+  return validateMail(mail) && validatePassword(password);
 };
 
 export const validateRegisterForm = ({ mail, username, password }) => {
-  const isMailValid = validateMail(mail);
-  const isPasswordValid = validatePassword(password);
-  const isUsernameValid = validateUsername(username);
-  return isMailValid && isPasswordValid && isUsernameValid;
+  return (
+    validateMail(mail) &&
+    validateUsername(username) &&
+    validatePassword(password)
+  );
 };
 
-const validatePassword = (password) => {
+export const validatePassword = (password) => {
   return password.length >= 6 && password.length <= 12;
 };
 
-const validateMail = (mail) => {
+export const validateMail = (mail) => {
   const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return emailPattern.test(mail);
 };
 
-const validateUsername = (username) => {
+export const validateUsername = (username) => {
   return username.length >= 3 && username.length <= 12;
 };
